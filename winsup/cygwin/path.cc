picolibc/picolibc

commit 43f65cdd7dae0bbeb5b5f26b008c8645d524869d

* Makefile.in (DLL_OFILES): Add fhandler_procsys.o. 	* devices.h (enum fh_devices): Add FH_PROCSYS. 	* devices.in (dev_procsys_storage): New device. 	* devices.cc: Regenerate. 	* dtable.cc (build_fh_pc): Add code to allocate fhandler_procsys. 	* fhandler.h (proc_len): Convert to size_t. 	(procsys): Declare. 	(procsys_len): Declare. 	(enum virtual_ftype_t): Move here from fhandler_virtual.h. 	Add members supported by fhandler_procsys. 	(fhandler_virtual::exists): Return virtual_ftype_t.  Change 	in all derived classes. 	(class fhandler_procsys): New class. 	(fhandler_union): Add fhandler_procnet and fhandler_procsys members. 	* fhandler_disk_file.cc (__DIR_mounts::check_missing_mount): Use 	ro_u_proc. 	(fhandler_base::fstat_by_handle): Don't copy attributes if file is an 	NT device. 	(fhandler_base::fstat_by_name): Ditto. 	* fhandler_netdrive.cc (fhandler_netdrive::exists): Return 	virtual_ftype_t. 	* fhandler_proc.cc (proc_tab): Sort alphabetically.  Use _VN macro 	to store length. 	(proc_len): Change to size_t. 	(proc_tab_cmp): New static function. 	(virt_tab_search): New function to search entry in virt_tab_t 	arrays.  Use throughout in /proc and sibling classes instead of 	loop. 	(fhandler_proc::exists): Return virtual_ftype_t. 	* fhandler_process.cc (process_tab): Sort alphabetically.  Use _VN 	macro to store length. 	(fhandler_process::exists): Return virtual_ftype_t. 	(fhandler_process::open): Simplify code. 	* fhandler_procnet.cc (procnet_tab): Sort alphabetically.  Use _VN 	macro to store length. 	(fhandler_procnet::exists): Return virtual_ftype_t. 	(fhandler_procnet::open): Simplify. 	* fhandler_procsys.cc: New file. 	* fhandler_registry.cc (fhandler_registry::exists): Return 	virtual_ftype_t. 	* fhandler_virtual.cc (fhandler_virtual::exists): Ditto. 	* fhandler_virtual.h (enum virtual_ftype_t): Move to fhandler.h. 	(virt_tab_t): Add name_len member. 	(_VN): New macro. 	(virt_tab_search): Declare. 	* mount.cc (mount_info::conv_to_win32_path): Fix comment.

winsup/cygwin/path.cc
@@ -761,8 +761,8 @@ path_conv::check (const char *src, unsigned opt,
 	    {
 	      /* FIXME: Calling build_fhandler here is not the right way to handle this. */
 	      fhandler_virtual *fh = (fhandler_virtual *) build_fh_dev (dev, path_copy);
-	      int file_type = fh->exists ();
-	      if (file_type == -2)
+	      virtual_ftype_t file_type = fh->exists ();
+	      if (file_type == virt_symlink)
 		{
 		  fh->fill_filebuf ();
 		  symlen = sym.set (fh->get_filebuf ());
@@ -770,30 +770,60 @@ path_conv::check (const char *src, unsigned opt,
 	      delete fh;
 	      switch (file_type)
 		{
-		  case 1:
-		  case 2:
+		  case virt_directory:
+		  case virt_rootdir:
 		    if (component == 0)
 		      fileattr = FILE_ATTRIBUTE_DIRECTORY;
 		    break;
-		  case -1:
+		  case virt_file:
 		    if (component == 0)
 		      fileattr = 0;
 		    break;
-		  case -2:	/* /proc/self or /proc/<pid>/symlinks */
+		  case virt_symlink:
 		    goto is_virtual_symlink;
-		  case -3:	/* /proc/<pid>/fd/pipe:[] */
+		  case virt_pipe:
 		    if (component == 0)
 		      {
 			fileattr = 0;
 			dev.parse (FH_PIPE);
 		      }
 		    break;
-		  case -4:	/* /proc/<pid>/fd/socket:[] */
+		  case virt_socket:
 		    if (component == 0)
 		      {
 			fileattr = 0;
 			dev.parse (FH_TCP);
 		      }
+		    break;
+		  case virt_fsdir:
+		  case virt_fsfile:
+		    /* Access to real file or directory via block device
+		       entry in /proc/sys.  Convert to real file and go with
+		       the flow. */
+		    dev.parse (FH_FS);
+		    goto is_fs_via_procsys;
+		  case virt_blk:
+		    /* Block special device.  If the trailing slash has been
+		       requested, the target is the root directory of the
+		       filesystem on this block device.  So we convert this to
+		       a real file and attach the backslash. */
+		    if (component || need_directory)
+		      {
+			dev.parse (FH_FS);
+			if (component == 0)
+			  {
+			    strcat (full_path, "\\");
+			    fileattr = FILE_ATTRIBUTE_DIRECTORY
+				       | FILE_ATTRIBUTE_DEVICE;
+			  }
+			else
+			  fileattr = 0;
+			goto out;
+		      }
+		    /*FALLTHRU*/
+		  case virt_chr:
+		    if (component == 0)
+		      fileattr = FILE_ATTRIBUTE_DEVICE;
 		    break;
 		  default:
 		    if (component == 0)
@@ -840,6 +870,8 @@ path_conv::check (const char *src, unsigned opt,
 	     casesensitive. */
 	  if (is_msdos)
 	    sym.pflags |= PATH_NOPOSIX | PATH_NOACL;
+
+is_fs_via_procsys:
 
 	  symlen = sym.check (full_path, suff, fs, conv_handle);
 
@@ -2850,6 +2882,14 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
 	    if (buf[1] != ':') /* native UNC path */
 	      *(buf += 2) = '\\';
 	  }
+	else if (*buf == '\\')
+	  {
+	    /* Device name points to somewhere else in the NT namespace. 
+	       Use GLOBALROOT prefix to convert to Win32 path. */
+	    char *p = stpcpy (buf, "\\\\.\\GLOBALROOT");
+	    sys_wcstombs (p, NT_MAX_PATH - (p - buf),
+			  up->Buffer, up->Length / sizeof (WCHAR));
+	  }
 	lsiz = strlen (buf) + 1;
 	/* TODO: Incoming "." is a special case which leads to a trailing
 	   backslash ".\\" in the Win32 path.  That's a result of the
@@ -2891,7 +2931,8 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
 	     quite a bunch of Win32 functions, especially in user32.dll,
 	     apparently, which don't grok long path names at all, not even
 	     in the UNICODE API. */
-	  if (lsiz <= MAX_PATH + 4 || (path[5] != L':' && lsiz <= MAX_PATH + 6))
+	  if ((path[5] == L':' && lsiz <= MAX_PATH + 4)
+	      || (!wcsncmp (path + 4, L"UNC\\", 4) && lsiz <= MAX_PATH + 6))
 	    {
 	      path += 4;
 	      lsiz -= 4;
@@ -2901,6 +2942,13 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
 		  lsiz -= 2;
 		}
 	    }
+	}
+      else if (*path == L'\\')
+	{
+	  /* Device name points to somewhere else in the NT namespace. 
+	     Use GLOBALROOT prefix to convert to Win32 path. */
+	  to = (void *) wcpcpy ((wchar_t *) to, L"\\\\.\\GLOBALROOT");
+	  lsiz += sizeof ("\\\\.\\GLOBALROOT") - 1;
 	}
       /* TODO: Same ".\\" band-aid as in CCP_POSIX_TO_WIN_A case. */
       if (relative && !strcmp ((const char *) from, ".")
@@ -2943,10 +2991,10 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
     case CCP_POSIX_TO_WIN_A:
     case CCP_WIN_A_TO_POSIX:
     case CCP_WIN_W_TO_POSIX:
-      strcpy ((char *) to, buf);
+      stpcpy ((char *) to, buf);
       break;
     case CCP_POSIX_TO_WIN_W:
-      wcscpy ((PWCHAR) to, path);
+      wcpcpy ((PWCHAR) to, path);
       break;
     }
   return 0;

winsup/cygwin/Makefile.in
@@ -141,13 +141,14 @@ DLL_OFILES:=assert.o autoload.o bsdlib.o ctype.o cxx.o cygheap.o cygthread.o \
 	fhandler.o fhandler_clipboard.o fhandler_console.o fhandler_disk_file.o \
 	fhandler_dsp.o fhandler_fifo.o fhandler_floppy.o fhandler_mailslot.o \
 	fhandler_mem.o fhandler_netdrive.o fhandler_nodevice.o fhandler_proc.o \
-	fhandler_process.o fhandler_procnet.o fhandler_random.o fhandler_raw.o \
-	fhandler_registry.o fhandler_serial.o fhandler_socket.o fhandler_tape.o \
-	fhandler_termios.o fhandler_tty.o fhandler_virtual.o fhandler_windows.o \
-	fhandler_zero.o flock.o fnmatch.o fork.o fts.o ftw.o getopt.o glob.o \
-	glob_pattern_p.o globals.o grp.o heap.o hookapi.o inet_addr.o inet_network.o \
-	init.o ioctl.o ipc.o kernel32.o libstdcxx_wrapper.o localtime.o lsearch.o \
-	malloc_wrapper.o minires-os-if.o minires.o miscfuncs.o mktemp.o mmap.o msg.o \
+	fhandler_process.o fhandler_procnet.o fhandler_procsys.o fhandler_random.o \
+	fhandler_raw.o fhandler_registry.o fhandler_serial.o fhandler_socket.o \
+	fhandler_tape.o fhandler_termios.o fhandler_tty.o fhandler_virtual.o \
+	fhandler_windows.o fhandler_zero.o flock.o fnmatch.o fork.o fts.o ftw.o \
+	getopt.o glob.o glob_pattern_p.o globals.o grp.o heap.o hookapi.o \
+	inet_addr.o inet_network.o init.o ioctl.o ipc.o kernel32.o \
+	libstdcxx_wrapper.o localtime.o lsearch.o malloc_wrapper.o \
+	minires-os-if.o minires.o miscfuncs.o mktemp.o mmap.o msg.o \
 	mount.o net.o netdb.o nfs.o nftw.o nlsfuncs.o ntea.o passwd.o path.o \
 	pinfo.o pipe.o poll.o posix_ipc.o pseudo-reloc.o pthread.o random.o \
 	regcomp.o regerror.o regexec.o regfree.o registry.o resource.o rexec.o \

winsup/cygwin/devices.cc
@@ -24,6 +24,9 @@ const device dev_proc_storage =
 const device dev_procnet_storage =
   {"", {FH_PROCNET}, ""};
 
+const device dev_procsys_storage =
+  {"", {FH_PROCSYS}, ""};
+
 const device dev_netdrive_storage =
   {"", {FH_NETDRIVE}, ""};
 

winsup/cygwin/devices.h
@@ -1,6 +1,6 @@
 /* devices.h
 
-   Copyright 2002, 2003, 2004, 2005 Red Hat, Inc.
+   Copyright 2002, 2003, 2004, 2005, 2007, 2009, 2010 Red Hat, Inc.
 
 This file is part of Cygwin.
 
@@ -53,6 +53,7 @@ enum fh_devices
   FH_DEV     = FHDEV (0, 245),
   FH_PROCNET = FHDEV (0, 244),
   FH_PROCESSFD = FHDEV (0, 243),
+  FH_PROCSYS = FHDEV (0, 242),
 
   DEV_FLOPPY_MAJOR = 2,
   FH_FLOPPY  = FHDEV (DEV_FLOPPY_MAJOR, 0),

winsup/cygwin/devices.in
@@ -20,6 +20,9 @@ const device dev_proc_storage =
 const device dev_procnet_storage =
   {"", {FH_PROCNET}, ""};
 
+const device dev_procsys_storage =
+  {"", {FH_PROCSYS}, ""};
+
 const device dev_netdrive_storage =
   {"", {FH_NETDRIVE}, ""};
 

winsup/cygwin/dtable.cc
@@ -540,6 +540,9 @@ build_fh_pc (path_conv& pc, bool set_name)
 	case FH_PROCNET:
 	  fh = cnew (fhandler_procnet) ();
 	  break;
+	case FH_PROCSYS:
+	  fh = cnew (fhandler_procsys) ();
+	  break;
 	case FH_NETDRIVE:
 	  fh = cnew (fhandler_netdrive) ();
 	  break;

winsup/cygwin/fhandler.h
@@ -34,7 +34,9 @@ extern const char *windows_device_names[];
 extern struct __cygwin_perfile *perfile_table;
 #define __fmode (*(user_data->fmode_ptr))
 extern const char proc[];
-extern const int proc_len;
+extern const size_t proc_len;
+extern const char procsys[];
+extern const size_t procsys_len;
 
 class select_record;
 class select_stuff;
@@ -97,6 +99,20 @@ enum del_lock_called_from {
   on_close,
   after_fork,
   after_exec
+};
+
+enum virtual_ftype_t {
+  virt_blk = -7,	/* Block special */
+  virt_chr = -6,	/* Character special */
+  virt_fsfile = -5,	/* FS-based file via /proc/sys */
+  virt_socket = -4,	/* Socket */
+  virt_pipe = -3,	/* Pipe */
+  virt_symlink = -2,	/* Symlink */
+  virt_file = -1,	/* Regular file */
+  virt_none = 0,	/* Invalid, Error */
+  virt_directory = 1,	/* Directory */
+  virt_rootdir = 2,	/* Root directory of virtual FS */
+  virt_fsdir = 3,	/* FS-based directory via /proc/sys */
 };
 
 class fhandler_base
@@ -1331,7 +1347,7 @@ class fhandler_virtual : public fhandler_base
   fhandler_virtual ();
   virtual ~fhandler_virtual();
 
-  virtual int exists();
+  virtual virtual_ftype_t exists();
   DIR *opendir (int fd) __attribute__ ((regparm (2)));
   long telldir (DIR *);
   void seekdir (DIR *, long);
@@ -1357,7 +1373,7 @@ class fhandler_proc: public fhandler_virtual
 {
  public:
   fhandler_proc ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   static DWORD get_proc_fhandler(const char *path);
 
@@ -1366,11 +1382,30 @@ class fhandler_proc: public fhandler_virtual
   bool fill_filebuf ();
 };
 
+class fhandler_procsys: public fhandler_virtual
+{
+ public:
+  fhandler_procsys ();
+  virtual_ftype_t exists(struct __stat64 *buf) __attribute__ ((regparm (2)));
+  virtual_ftype_t exists();
+  DIR *opendir (int fd) __attribute__ ((regparm (2)));
+  int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
+  long telldir (DIR *);
+  void seekdir (DIR *, long);
+  int closedir (DIR *);
+  int open (int flags, mode_t mode = 0);
+  int close ();
+  void __stdcall read (void *ptr, size_t& len) __attribute__ ((regparm (3)));
+  ssize_t __stdcall write (const void *ptr, size_t len);
+  int __stdcall fstat (struct __stat64 *buf) __attribute__ ((regparm (2)));
+  bool fill_filebuf ();
+};
+
 class fhandler_netdrive: public fhandler_virtual
 {
  public:
   fhandler_netdrive ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   void seekdir (DIR *, long);
   void rewinddir (DIR *);
@@ -1388,7 +1423,7 @@ class fhandler_registry: public fhandler_proc
  public:
   fhandler_registry ();
   void set_name (path_conv &pc);
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   long telldir (DIR *);
   void seekdir (DIR *, long);
@@ -1408,7 +1443,7 @@ class fhandler_process: public fhandler_proc
   pid_t pid;
  public:
   fhandler_process ();
-  int exists();
+  virtual_ftype_t exists();
   DIR *opendir (int fd) __attribute__ ((regparm (2)));
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   int open (int flags, mode_t mode = 0);
@@ -1421,7 +1456,7 @@ class fhandler_procnet: public fhandler_proc
   pid_t pid;
  public:
   fhandler_procnet ();
-  int exists();
+  virtual_ftype_t exists();
   int readdir (DIR *, dirent *) __attribute__ ((regparm (3)));
   int open (int flags, mode_t mode = 0);
   int __stdcall fstat (struct __stat64 *buf) __attribute__ ((regparm (2)));
@@ -1462,6 +1497,8 @@ typedef union
   char __pipe[sizeof (fhandler_pipe)];
   char __proc[sizeof (fhandler_proc)];
   char __process[sizeof (fhandler_process)];
+  char __procnet[sizeof (fhandler_procnet)];
+  char __procsys[sizeof (fhandler_procsys)];
   char __pty_master[sizeof (fhandler_pty_master)];
   char __registry[sizeof (fhandler_registry)];
   char __serial[sizeof (fhandler_serial)];

winsup/cygwin/fhandler_disk_file.cc
@@ -118,7 +118,7 @@ public:
 	    {
 	      found[__DIR_PROC] = true;
 	      if (retname)
-		RtlInitUnicodeString (retname, L"proc");
+		*retname = ro_u_proc;
 	      return 2;
 	    }
 	  if (!found[__DIR_CYGDRIVE])
@@ -407,7 +407,9 @@ fhandler_base::fstat_by_handle (struct __stat64 *buf)
      entry, as in other calls to fstat_helper. */
   if (pc.is_rep_symlink ())
     fi.fbi.FileAttributes &= ~FILE_ATTRIBUTE_DIRECTORY;
-  pc.file_attributes (fi.fbi.FileAttributes);
+  /* Only copy attributes if not a device root dir. */
+  if (!(pc.file_attributes () & FILE_ATTRIBUTE_DEVICE))
+    pc.file_attributes (fi.fbi.FileAttributes);
   return fstat_helper (buf,
 		       fi.fbi.ChangeTime.QuadPart ? &fi.fbi.ChangeTime
 						  : &fi.fbi.LastWriteTime,
@@ -474,7 +476,9 @@ fhandler_base::fstat_by_name (struct __stat64 *buf)
      entry, as in other calls to fstat_helper. */
   if (pc.is_rep_symlink ())
     fdi_buf.fdi.FileAttributes &= ~FILE_ATTRIBUTE_DIRECTORY;
-  pc.file_attributes (fdi_buf.fdi.FileAttributes);
+  /* Only copy attributes if not a device root dir. */
+  if (!(pc.file_attributes () & FILE_ATTRIBUTE_DEVICE))
+    pc.file_attributes (fdi_buf.fdi.FileAttributes);
   return fstat_helper (buf,
 		       fdi_buf.fdi.ChangeTime.QuadPart
 		       ? &fdi_buf.fdi.ChangeTime : &fdi_buf.fdi.LastWriteTime,

jeremyd2019/msys2-runtime

commit 7f2444ba1e5a5544ea246cc74af80e7536752c13

Cygwin: cygwin_conv_path: don't write to `to` before size is validated.
In the CCP_POSIX_TO_WIN_W path, when `from` is a device,
cygwin_conv_path would attempt to write to the `to` buffer before the
validation of the `size`.  This resulted in an EFAULT error in the
common use-case of passing `to` as NULL and `size` as 0 to get the
required size of `to` for the conversion (as used in
cygwin_create_path).  Instead, set a boolean and write to `to`
after validation.

Fixes: 43f65cdd7dae ("* Makefile.in (DLL_OFILES): Add fhandler_procsys.o.")
Addresses: https://cygwin.com/pipermail/cygwin/2025-April/258068.html
Signed-off-by: Jeremy Drake <cygwin@jdrake.com>

winsup/cygwin/path.cc
@@ -4239,6 +4239,7 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
   int how = what & CCP_CONVFLAGS_MASK;
   what &= CCP_CONVTYPE_MASK;
   int ret = -1;
+  bool prependglobalroot = false;
 
   __try
     {
@@ -4347,7 +4348,7 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
 	    {
 	      /* Device name points to somewhere else in the NT namespace.
 		 Use GLOBALROOT prefix to convert to Win32 path. */
-	      to = (void *) wcpcpy ((wchar_t *) to, ro_u_globalroot.Buffer);
+	      prependglobalroot = true;
 	      lsiz += ro_u_globalroot.Length / sizeof (WCHAR);
 	    }
 	  /* TODO: Same ".\\" band-aid as in CCP_POSIX_TO_WIN_A case. */
@@ -4403,6 +4404,8 @@ cygwin_conv_path (cygwin_conv_path_t what, const void *from, void *to,
 	  stpcpy ((char *) to, buf);
 	  break;
 	case CCP_POSIX_TO_WIN_W:
+	  if (prependglobalroot)
+	    to = (void *) wcpcpy ((PWCHAR) to, ro_u_globalroot.Buffer);
 	  wcpcpy ((PWCHAR) to, path);
 	  break;
 	}