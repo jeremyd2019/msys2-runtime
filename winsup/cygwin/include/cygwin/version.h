724ba9bd82205ef08b0672cecc45a263f5bd9fe0
Jeremy Drake <github@jdrake.com>
2025-07-31 17:05:28 -0700

    test api major version 1

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -497,8 +497,8 @@ details. */
   Note that we forgot to bump the api for ualarm, strtoll, strtoull,
   sigaltstack, sethostname. */
 
-#define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 357
+#define CYGWIN_VERSION_API_MAJOR 1
+#define CYGWIN_VERSION_API_MINOR 359
 
 /* There is also a compatibity version number associated with the shared memory
    regions.  It is incremented when incompatible changes are made to the shared
